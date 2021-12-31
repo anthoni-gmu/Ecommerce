2905a83330e33abb590d28a35207438fb6c2dc09
anthoni_gmu <softwaretoni21@gmail.com>
2021-12-31 16:29:36 -0500

    first commit

---
+++ apps/user/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+
+import os 
+
+
+class UserAccountManager(BaseUserManager):
+    def create_user(self,email,password=None,**estra_fields):
+        if not email:
+            raise ValueError('users must have an email address')
+        
+        email = self.normalize_email(email)
+        user =self.model(email=email,**estra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+    
+    def create_superuser(self,email,password,**estra_fields):
+        user =self.create_user(email,password=password,**estra_fields)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+class UserAccount(AbstractBaseUser,PermissionsMixin):
+    email=models.EmailField(max_length=255,unique=True)
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['first_name','last_name']
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
+    def __str__(self):
+        return self.email
+
+
+
+        
+
