93f50dfc84c8a4d9811f90492de9873bc14b6fe8
anthoni_gmu <softwaretoni21@gmail.com>
2021-12-31 23:43:09 -0500

    auth_js

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.social.urls')),
+
     path('admin/', admin.site.urls),
+   
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
