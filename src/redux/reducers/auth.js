f47d5ecb73663a36d054113eb8f5fb40eaea07a2
anthoni_gmu <softwaretoni21@gmail.com>
2022-01-02 23:54:18 -0500

    activate

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,4 +1,11 @@
-import {SIGNUP_SUCCESS ,SIGNUP_FAIL} from '../actions/types'
+import {
+    SIGNUP_SUCCESS ,
+    SIGNUP_FAIL,
+    ACTIVATION_SUCCESS,
+    ACTIVATION_FAIL,
+    SET_AUTH_LOADING,
+    REMOVE_AUTH_LOADING
+} from '../actions/types'
 
 const initialState = {
     access: localStorage.getItem('access'),
@@ -12,8 +19,22 @@ export default function Auth(state = initialState, action){
     const {type,payload} = action
 
     switch (type) {
+        case SET_AUTH_LOADING:
+            return {
+                ...state,
+                loading:true
+            }
+        case REMOVE_AUTH_LOADING:
+            return {
+                ...state,
+                loading:false
+            }
+        case ACTIVATION_SUCCESS:
+        case ACTIVATION_FAIL:
+            return{
+                ...state
+            }
         case SIGNUP_SUCCESS:
-            
         case SIGNUP_FAIL:
             localStorage.removeItem('access')
             localStorage.removeItem('refresh')
